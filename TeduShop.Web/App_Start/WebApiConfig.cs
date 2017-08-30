0b1c8b705573278ad509f511c6b6450446b56d64
LONG-PC\LonG-PC <longit93@gmail.com>
2017-08-30 15:09:11 +0700

    how to use post

--- TeduShop.Web/App_Start/WebApiConfig.cs
+++ TeduShop.Web/App_Start/WebApiConfig.cs
@@ -18,7 +18,7 @@ namespace TeduShop.Web
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
-            );
+                );
         }
     }
 }
